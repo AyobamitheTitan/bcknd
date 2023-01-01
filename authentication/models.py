45b783df36bc465dd92194ba111f6fb7246cf45e
Oluwatimilehin <ayobamijijnr@gmail.com>
2023-01-01 20:33:56 +0100

    inherit from abstract class

--- authentication/models.py
+++ authentication/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
-class UserModel(models.Model):
+class UserModel(AbstractBaseUser):
     id = models.UUIDField(primary_key=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
