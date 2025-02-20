ad8beb9b4115b340487b003ec9ed6791960397a6
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-02-20 14:40:53 +0100

    utilize environment variables to hide secrets

--- app/settings.py
+++ app/settings.py
@@ -11,20 +11,24 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from datetime import timedelta
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yn^$_d9sgzmxh*gspmxrxust5ud2ssifnuw1%lv!)4q_3ebam9'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG",False)
 
 ALLOWED_HOSTS = []
 
@@ -84,11 +88,11 @@ DATABASES = {
     # },
     'default':{
         "ENGINE":"django.db.backends.postgresql",
-        "NAME":"bcknd",
-        "USER":"pomegranat",
-        "PASSWORD":"anewpomegranat",
-        "HOST":"localhost",
-        "PORT":5432
+        "NAME":os.getenv("DATABASE_NAME"),
+        "USER":os.getenv("DATABASE_USER"),
+        "PASSWORD":os.getenv("DATABASE_PASSWORD"),
+        "HOST":os.getenv("DATABASE_HOST"),
+        "PORT":os.getenv("DATABASE_PORT")
     }
 }
 
