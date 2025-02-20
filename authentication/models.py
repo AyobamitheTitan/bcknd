568b3e13942f3c21b51d2b5ccc645ac2b08f19f4
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-02-20 11:47:03 +0100

    update password length and add username column. Username is required by the abstract class

--- authentication/models.py
+++ authentication/models.py
@@ -8,7 +8,18 @@ class UserModel(AbstractBaseUser):
     last_name = models.CharField(max_length=30)
     email = models.CharField(max_length=100, unique=True)
     phone_number = models.CharField(unique=True, max_length=15)
-    password = models.CharField(max_length=100)
+    password = models.CharField(max_length=150)
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        help_text=(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        # validators=[username_validator],   
+    )
+
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = "username"
 
     def __str__(self):
         return f"User [ id: {self.id}, email: {self.email}]"
