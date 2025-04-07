0426efc55aef3a5d894936e2987adb1e3e2de86a
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-04-07 07:32:05 +0100

    catch unauthenticated error

--- app/utils.py
+++ app/utils.py
@@ -1,12 +1,11 @@
 from rest_framework.views import exception_handler
 from rest_framework.response import Response
-from rest_framework.exceptions import ParseError
+from rest_framework.exceptions import ParseError, NotAuthenticated
 from rest_framework import status
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
 
-    
     if isinstance(exc, ParseError):
         return Response(
             {
@@ -14,6 +13,15 @@ def custom_exception_handler(exc, context):
                 "details": {"body":["Check your JSON syntax and try again."]}
             },
             status=status.HTTP_400_BAD_REQUEST
+        )
+    if isinstance(exc, NotAuthenticated):
+        return Response({
+            "error":"Not authenticated",
+            "details": {
+                "authorization": [response.data.get("detail")]
+                }
+            },
+            status=status.HTTP_401_UNAUTHORIZED
         )
 
     return response
