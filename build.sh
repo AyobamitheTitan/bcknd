1581cb47e54e1fae358a7e985235a5e48f73e451
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-04-07 07:31:33 +0100

    update build command

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+# Make new migrations
+python manage.py makemigrations
 
 # Apply any outstanding database migrations
 python manage.py migrate
