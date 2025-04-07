38314fb853bedb0da90333512576fc5937b5e0d1
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-04-07 07:31:16 +0100

    add bin urls

--- app/urls.py
+++ app/urls.py
@@ -37,6 +37,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/auth/",include("authentication.urls")),
+    path("api/bins",include("bins.urls")),
     path("", BaseView.as_view()),
     path('swagger/docs', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
