ab9ae2c2678e3648c3861c657fe3bfd4de297d5f
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-04-09 18:36:28 +0100

    update models

--- bins/models.py
+++ bins/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class BinLocationModel(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    location = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    capacity = models.IntegerField()
+
 class BinModel(models.Model):
     id = models.UUIDField(primary_key=True)
-    location = models.CharField(max_length=100)
+    location = models.ForeignKey(
+        BinLocationModel,
+        on_delete=models.CASCADE,
+        related_name="bins"
+    )
+    bin_url = models.CharField(max_length=150, default=None)
     emptied_at = models.DateTimeField(null=True)
-    fill_level = models.FloatField()
-
-
-# class BinLocation(models.Model):
-#     id = models.UUIDField(primary_key=True)
-#     location = models.CharField(max_length=100)
-#     number_of_bins = models.BigIntegerField(null=False)
-#     date_emptied = models
