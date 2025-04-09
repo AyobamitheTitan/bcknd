903a23b8259468a7e5ae25524467b7385be87caf
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-04-09 18:35:35 +0100

    new errors to be caught

--- app/utils.py
+++ app/utils.py
@@ -1,16 +1,35 @@
+from requests.exceptions import ConnectionError
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.views import exception_handler
 from rest_framework.response import Response
-from rest_framework.exceptions import ParseError, NotAuthenticated
+from rest_framework.exceptions import (
+    APIException,
+    ParseError, 
+    NotAuthenticated,
+    ValidationError,
+    MethodNotAllowed,
+)
+from rest_framework_simplejwt.exceptions import InvalidToken
 from rest_framework import status
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
 
+    if isinstance(exc, ConnectionError):
+        return Response({
+            "error":"Connection Timeout",
+            "details":{
+                "network":["Network request timed out. Check your internet connection."]
+            }
+        },
+        status=status.HTTP_408_REQUEST_TIMEOUT
+    )
     if isinstance(exc, ParseError):
         return Response(
             {
                 "error": "Invalid JSON format",
-                "details": {"body":["Check your JSON syntax and try again."]}
+                "details": {
+                    "body":["Check your JSON syntax and try again."]}
             },
             status=status.HTTP_400_BAD_REQUEST
         )
@@ -23,5 +42,35 @@ def custom_exception_handler(exc, context):
             },
             status=status.HTTP_401_UNAUTHORIZED
         )
+    if isinstance(exc, InvalidToken):
+        return Response({
+            "error":"Invalid token",
+            "details":{
+                "token":"Token is either invalid or expired"
+            }
+        },
+        status=status.HTTP_401_UNAUTHORIZED
+    )
+    if isinstance(exc, ValidationError):
+        return Response({
+            "error":"Invalid data received",
+            "details":exc.get_full_details()
+        },
+            status=status.HTTP_400_BAD_REQUEST
+    )
+    if issubclass(type(exc), ObjectDoesNotExist):
+        return Response({
+            "error":"Not found",
+            "details":{str(exc)}
+        },
+            status=status.HTTP_404_NOT_FOUND
+    )
+    if issubclass(type(exc), MethodNotAllowed):
+        return Response({
+            "error":"Method not allowed",
+            "details":"Please correct the API verb in use and try again"
+        },
+        status=status.HTTP_405_METHOD_NOT_ALLOWED
+    )
 
     return response
